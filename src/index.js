6b4ef760ce1e70322ff8dd22ec5e8e1e0a287ff2
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-09-15 16:51:27 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { findProduct } from '';
 const container = document.querySelector('.js-container');
 
 function createMarkup(arr) {
-  const markup = arr.map(item => ``).join('');
+  const markup = arr
+    .map(
+      item => `<li class="js-list-item"><img src="" alt="">
+      <h2></h2>
+      <h3></h3>
+      <p></p>
+      <div><button class="li-btn" type="button">Add to Favorite</button><button class="li-btn" type="button"> Add to Basket</button></div></li>`
+    )
+    .join('');
   container.innerHTML = markup;
 }
